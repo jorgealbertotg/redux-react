2d9ff58309840bd388af5ed4f7b8b1847e958cd4
jota <jorge-alberto.tolentino-garcia@gft.com>
2020-07-17 13:47:39 -0500

    Adding a reducer

--- src/App.js
+++ src/App.js
@@ -1,22 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
+import { connect } from 'react-redux';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <div>
-          <p>Age: <span></span></p>
+class App extends Component {
+  // state = { age: 21 };
+
+  // onAgeUp = () => {
+  //   this.setState({
+  //     ...this.state,
+  //     age: ++this.state.age
+  //   });
+  // }
+
+  // onAgeDown = () => {
+  //   this.setState({
+  //     ...this.state,
+  //     age: --this.state.age
+  //   });
+  // }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
           <div>
-            <button>Age UP</button>
-            <button>Age DOWN</button>
+            <p>Age from store: <span>{this.props.age}</span></p>
+            <div>
+              <button onClick={this.props.onAgeUp}>Age UP</button>
+              <button onClick={this.props.onAgeDown}>Age DOWN</button>
+            </div>
           </div>
-        </div>
-      </header>
-    </div>
-  );
+        </header>
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = (state) =>{
+  return {
+    age: state.age
+  };
+}
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onAgeUp: () => dispatch({type: 'AGE_UP'}),
+    onAgeDown: () => dispatch({type: 'AGE_DOWN'})
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
