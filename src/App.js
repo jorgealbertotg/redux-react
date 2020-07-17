e821d730eadf8192ecc66300b3bf59828c390712
jota <jorge-alberto.tolentino-garcia@gft.com>
2020-07-17 13:06:34 -0500

    Adding state

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <div>
-          <p>Age: <span></span></p>
+class App extends Component {
+  state = { age: 21 };
+
+  onAgeUp = () => {
+    this.setState({
+      ...this.state,
+      age: ++this.state.age
+    });
+  }
+
+  onAgeDown = () => {
+    this.setState({
+      ...this.state,
+      age: --this.state.age
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
           <div>
-            <button>Age UP</button>
-            <button>Age DOWN</button>
+            <p>Age: <span>{this.state.age}</span></p>
+            <div>
+              <button onClick={this.onAgeUp}>Age UP</button>
+              <button onClick={this.onAgeDown}>Age DOWN</button>
+            </div>
           </div>
-        </div>
-      </header>
-    </div>
-  );
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
