38efd92d65cb32edf384aa7920f2bd63be02d273
jota <jorge-alberto.tolentino-garcia@gft.com>
2020-07-17 17:30:44 -0500

    Adding thunk middleware for asynchronous calls

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
+import { connect } from 'react-redux';
+import * as actionCreator from './store/actions/actions';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <div>
-          <p>Age: <span></span></p>
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
           <div>
-            <button>Age UP</button>
-            <button>Age DOWN</button>
+            <p>Age from store: <span>{this.props.age}</span></p>
+            <div>
+              <button onClick={this.props.onAgeUp}>Age UP</button>
+              <button onClick={this.props.onAgeDown}>Age DOWN</button>
+              {this.props.loading && <img src={logo} alt="logo" className="App-logo" />}
+            </div>
           </div>
-        </div>
-      </header>
-    </div>
-  );
+        </header>
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = (state) =>{
+  return {
+    age: state.age,
+    loading: state.loading
+  };
+}
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onAgeUp: () => dispatch(actionCreator.ageUp(1)),
+    onAgeDown: () => dispatch(actionCreator.ageDown(1))
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -4,10 +4,18 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reducer from './store/reducer';
+import thunk from 'redux-thunk';
+
+const store = createStore(reducer, applyMiddleware(thunk));
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
